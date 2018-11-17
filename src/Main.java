94f0e323ab0ba491f6ab4c8eae5c477b55b3367d
Sébastien Viguier <viguier.sebastien@orange.fr>
2018-11-17 16:09:35 +0100

    adding .idea

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        List<String[]> dominos = CSVParser.parse("./123.csv", ",", true);
+        List<String[]> dominos = CSVParser.parse("./dominos.csv", ",", true);
 
         for(String[] strs : dominos){
             System.out.println(Arrays.toString(strs));
