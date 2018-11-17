13c69f8196c0c8f61b756f17fea8f8b289faa0d2
Hmatico <mathieu.valentin1@orange.fr>
2018-11-17 17:38:50 +0100

    Ajout classe Tests

--- src/test/ApplicationTest.java
+++ src/test/ApplicationTest.java
@@ -1,7 +1,17 @@
 package test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Test;
+import plateau.Case;
 
-class ApplicationTest {
+import static org.junit.Assert.*;
+
+public class ApplicationTest {
+
+    @Test
+    public void CaseTest(){
+        Case casePlateau = new Case(1,1);
+        assertTrue(casePlateau.getX() == 1);
+        assertFalse(casePlateau.getY() == 2);
+    }
 
 }
