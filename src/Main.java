34afa56ab3eab066048cd5bd79ff0b152d70c124
Sébastien Viguier <viguier.sebastien@orange.fr>
2018-11-17 15:53:42 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import util.CSVParser;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        List<String[]> dominos = CSVParser.parse("./123.csv", ",", true);
+
+        for(String[] strs : dominos){
+            System.out.println(Arrays.toString(strs));
+        }
+    }
+}
---
+++ src/util/CSVParser.java
@@ -0,0 +1,28 @@
+package util;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CSVParser {
+
+    public static List<String[]> parse(String filePath, String delimiter, boolean ignoreFirstLine) throws IOException {
+        BufferedReader file = new BufferedReader(new FileReader(filePath));
+        String line = null;
+        List<String[]> result = new ArrayList<String[]>();
+
+        if(ignoreFirstLine){
+            file.readLine();
+        }
+
+        while((line = file.readLine()) != null){
+            result.add(line.split(delimiter));
+        }
+
+        try{ file.close(); } catch (IOException e){}
+
+        return result;
+    }
+}
