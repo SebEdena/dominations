a00c427cb0ecedabd68d39c7de7a3290d0a74aa7
Hmatico <mathieu.valentin1@orange.fr>
2018-11-19 18:42:25 +0100

    class Jeu

--- src/jeu/Jeu.java
+++ src/jeu/Jeu.java
@@ -1,9 +1,19 @@
 package jeu;
 
+import plateau.Plateau;
+
+import java.util.Map;
+
 public class Jeu {
-    /*
-    --
-    --
-    --
-     */
+
+    private Map<Joueur, Plateau> joueurs;
+
+    public Jeu(){
+
+    }
+
+    public Jeu(int nbJoueurs){
+
+    }
+
 }
