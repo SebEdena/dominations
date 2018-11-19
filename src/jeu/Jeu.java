eb2a60f23aeff988aec8d52e27a37d173216f957
Hmatico <mathieu.valentin1@orange.fr>
2018-11-19 19:10:20 +0100

    class Jeu en travaux

--- src/jeu/Jeu.java
+++ src/jeu/Jeu.java
@@ -3,17 +3,43 @@ package jeu;
 import plateau.Plateau;
 
 import java.util.Map;
+import java.util.Scanner;
 
 public class Jeu {
+    private static final int SCORE_DEFAUT = 0;
+    private static final int NB_JOUEURS_MIN = 2;
+    private static final int NB_JOUEURS_MAX = 4;
+
+    private static final int GRAND_PLATEAU = 7;
+    private static final int PETIT_PLATEAU = 5;
+
+    private static Scanner scan = new Scanner(System.in);
 
     private Map<Joueur, Plateau> joueurs;
+    private int nbJoueurs;
 
-    public Jeu(){
+    private Jeu(){
+        int nb = intitialisationJeu();
+    }
+
+    private int intitialisationJeu() {
+        int nombre = 0;
+        do {
+            try{
+                System.out.println("Nombre de joueurs ? (2 à 4)");
+                nombre = Integer.parseInt(scan.next());
+            } catch (Exception e){
+                System.out.println(e.getMessage());
+            }
+        } while(nombre < NB_JOUEURS_MIN || nombre > NB_JOUEURS_MAX);
+        return nbJoueurs;
+    }
+
+    private void jeuDe3a4() {
 
     }
 
-    public Jeu(int nbJoueurs){
-
+    private void jeuA2() {
     }
 
 }
