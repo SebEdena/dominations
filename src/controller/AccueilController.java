a87f6bd66030034a6bc45198628f1ae8f8c348bb
SebEdena <viguier.sebastien@orange.fr>
2018-12-31 15:33:14 +0100

    Ajout Accueil Simple

--- src/controller/AccueilController.java
+++ src/controller/AccueilController.java
@@ -1,4 +1,24 @@
 package controller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+
 public class AccueilController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private ImageView accueilPlayButton;
+
+    @FXML
+    void initialize() {
+        assert accueilPlayButton != null : "fx:id=\"accueilPlayButton\" was not injected: check your FXML file 'accueil.fxml'.";
+
+    }
 }
