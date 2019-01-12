6482d5cb7ec28b9069774d04f493baa186368fc1
StepManlau <laurentyu1997@hotmail.fr>
2019-01-12 17:44:47 +0100

    ajout du mode de jeu

--- src/jeu/ModeJeu.java
+++ src/jeu/ModeJeu.java
@@ -1,20 +1,28 @@
 package jeu;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static jeu.NbJoueur.*;
+
 public enum ModeJeu {
-    STANDARD("Standard", 5, 0, 0),
-    EMPIRE_DU_MILIEU("Empire du Milieu", 5, 10, 0),
-    HARMONIE("Harmonie", 5, 0, 5);
+    STANDARD("Standard", 5, 0, 0, Arrays.asList()),
+    GRAND_DUEL("Grand Duel",7,0,0, Arrays.asList(jeuA3,jeuA4)),
+    EMPIRE_DU_MILIEU("Empire du Milieu", 5, 10, 0, Arrays.asList()),
+    HARMONIE("Harmonie", 5, 0, 5, Arrays.asList());
 
     private String libelle;
     private int taillePlateau;
     private int bonusPlacementMilieu;
     private int bonusPlateauComplet;
+    private List<NbJoueur> listerefuse;
 
-    ModeJeu(String libelle, int taillePlateau, int bonusPlacementMilieu, int bonusPlateauComplet){
+    ModeJeu(String libelle, int taillePlateau, int bonusPlacementMilieu, int bonusPlateauComplet, List<NbJoueur> listerefuse){
         this.libelle = libelle;
         this.taillePlateau = taillePlateau;
         this.bonusPlacementMilieu = bonusPlacementMilieu;
         this.bonusPlateauComplet = bonusPlateauComplet;
+        this.listerefuse = listerefuse;
     }
 
     public String getLibelle(){
@@ -23,5 +31,9 @@ public enum ModeJeu {
 
     public int getTaillePlateau() {
         return taillePlateau;
+    }
+
+    public List<NbJoueur> getListerefuse() {
+        return this.listerefuse;
     }
 }
