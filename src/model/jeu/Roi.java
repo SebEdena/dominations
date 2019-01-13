2f9bd200fe7280b18a323f5f05aafb9aad427749
Hmatico <mathieu.valentin1@orange.fr>
2019-01-13 19:53:20 +0100

    model.jeu ok

--- src/model/jeu/Roi.java
+++ src/model/jeu/Roi.java
@@ -1,3 +1,8 @@
+/**
+ * Classe permettant de décrire le roi
+ * @author Mathieu Valentin, Sébastien Viguier, Laurent Yu
+ * @version 1.0
+ */
 package model.jeu;
 
 import java.io.Serializable;
@@ -12,11 +17,21 @@ public enum Roi implements Serializable
     private String libelle;
     private String hexColor;
 
+    /**
+     * Constructeur de la classe d'énumération du roi
+     * @param libelle Couleur du roi
+     * @param hexColor Code couleur du roi en héxadécimal
+     */
     Roi(String libelle, String hexColor){
         this.libelle = libelle;
         this.hexColor = hexColor;
     }
 
+    /**
+     * Methode retournant le roi
+     * @param enumCouleur Index de la couleur
+     * @return Le roi choisi
+     */
     public static Roi getRoiInt(int enumCouleur)
     {
         switch(enumCouleur)
@@ -34,6 +49,11 @@ public enum Roi implements Serializable
         }
     }
 
+    /**
+     * Methode retournant l'index du roi
+     * @param roi Roi dont l'index est cherché
+     * @return L'index du roi cherché
+     */
     public static int getRoiIndex(Roi roi){
         for(int i = 0; i < values().length; i++){
             if(roi.equals(values()[i])) return i;
@@ -41,6 +61,11 @@ public enum Roi implements Serializable
         return -1;
     }
 
+    /**
+     * Methode retournant le roi en fonction d'une couleur
+     * @param color Couleur du roi cherché
+     * @return Roi de la couleur
+     */
     public static Roi getByColor(String color){
         for(Roi r : values()){
             if(r.getColor().equals(color)) return r;
@@ -48,10 +73,18 @@ public enum Roi implements Serializable
         return null;
     }
 
+    /**
+     * Methode retournant la couleur du roi
+     * @return La couleur du roi
+     */
     public String getLibelle() {
         return libelle;
     }
 
+    /**
+     * Methode retournant la couleur du roi en héxadécimal
+     * @return La couleur du roi en héxadécimal
+     */
     public String getColor(){
         return hexColor;
     }
