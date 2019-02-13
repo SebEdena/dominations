24f92b4ce91e88e32bad0ccc288fdf0f1d8f8ce5
StepManlau <laurentyu1997@hotmail.fr>
2019-02-13 19:13:29 +0100

    fonction potentiel + classe

---
+++ src/model/iaContest/Potentiel.java
@@ -0,0 +1,59 @@
+package model.iaContest;
+
+import model.plateau.Terrain;
+
+public class Potentiel
+{
+    private int nbCouronne;
+    private Terrain terrain;
+    private int nbCases;
+    private int potentiel;
+
+    public Potentiel(Terrain terrain)
+    {
+        nbCouronne = 0;
+        nbCases = 0;
+        this.terrain = terrain;
+        potentiel = 0;
+    }
+
+    public Potentiel(Terrain terrain, int nbCou, int nbCas)
+    {
+        nbCouronne = nbCou;
+        nbCases = nbCas;
+        this.terrain = terrain;
+        potentiel = 0;
+    }
+    
+    public int getNbCouronne() {
+        return nbCouronne;
+    }
+
+    public void setNbCouronne(int nbCouronne) {
+        this.nbCouronne = nbCouronne;
+    }
+
+    public Terrain getTerrain() {
+        return terrain;
+    }
+
+    public void setTerrain(Terrain terrain) {
+        this.terrain = terrain;
+    }
+
+    public int getNbCases() {
+        return nbCases;
+    }
+
+    public void setNbCases(int nbCases) {
+        this.nbCases = nbCases;
+    }
+
+    public int getPotentiel() {
+        return potentiel;
+    }
+
+    public void setPotentiel(int potentiel) {
+        this.potentiel = potentiel;
+    }
+}
--- src/model/iaContest/Utils.java
+++ src/model/iaContest/Utils.java
@@ -5,6 +5,12 @@ import grooptown.ia.model.GameState;
 import grooptown.ia.model.Kingdom;
 import grooptown.ia.model.Move;
 import grooptown.ia.model.Player;
+import model.plateau.Case;
+import model.plateau.Orientation;
+import model.plateau.Terrain;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Utils {
 
@@ -72,5 +78,61 @@ public class Utils {
         int moveScore = Score.getCrownScore(p);
         if(move.getPlacedDomino() != null) p.resetPlacedDomino();
         return moveScore == bestScore;
+    }
+
+    public static ArrayList<Potentiel> getPotentiels(Plateau p)
+    {
+        ArrayList<Potentiel> listePotentiels = new ArrayList<Potentiel>();
+        Case[][] tableau = p.getTableau();
+        List<Case> pileCasesVisitees = new ArrayList<Case>();
+        int NB_COL_LIG = Plateau.getNbColLig();
+        for(int i = 0; i < NB_COL_LIG; i++)
+        {
+            for(int j = 0 ; j < NB_COL_LIG; j++)
+            {
+                if(tableau[i][j] != null && !pileCasesVisitees.contains(tableau[i][j]) &&
+                        !tableau[i][j].getTerrain().equals(Terrain.CHATEAU))
+                {
+                    Case caseTemoin = tableau[i][j];
+                    List<Case> casesSimilaires = new ArrayList<Case>();
+                    rechercheCaseSimilaire(p,i,j, pileCasesVisitees, caseTemoin, casesSimilaires);
+
+                    int couronne = 0;
+                    int compteurCase = 0;
+                    for(Case c : casesSimilaires)
+                    {
+                        compteurCase++;
+                        couronne = couronne + c.getNbCouronne();
+                    }
+                    Terrain terrain = caseTemoin.getTerrain();
+                    listePotentiels.add(new Potentiel(terrain,couronne,compteurCase));
+                }
+            }
+        }
+        return listePotentiels;
+    }
+
+    private static void rechercheCaseSimilaire(Plateau p, int x, int y, List<Case> pileCasesVisitees, Case caseTemoin, List<Case> casesAdjacentes)
+    {
+        Case[][] tableau = p.getTableau();
+        int NB_COL_LIG = Plateau.getNbColLig();
+        casesAdjacentes.add(caseTemoin);
+        pileCasesVisitees.add(caseTemoin);
+        List<Case> casesTrouvees = new ArrayList<Case>();
+
+        for(Orientation o : Orientation.values()) // à chaque orientation on vérifie s'il y a une case de même domaine et non comptabilisée
+        {
+            if(x + o.getOffsetX() >= 0 && x + o.getOffsetX() < NB_COL_LIG &&
+                    y + o.getOffsetY() >= 0 && y + o.getOffsetY() < NB_COL_LIG &&
+                    tableau[x + o.getOffsetX()][y + o.getOffsetY()] != null &&
+                    tableau[x + o.getOffsetX()][y + o.getOffsetY()].getTerrain().equals(caseTemoin.getTerrain()) &&
+                    !pileCasesVisitees.contains(tableau[x + o.getOffsetX()][y + o.getOffsetY()]))
+            {
+                casesTrouvees.add(tableau[x + o.getOffsetX()][y + o.getOffsetY()]);
+                pileCasesVisitees.add(tableau[x + o.getOffsetX()][y + o.getOffsetY()]);
+                rechercheCaseSimilaire(p,x + o.getOffsetX(), y + o.getOffsetY(),
+                        pileCasesVisitees,tableau[x + o.getOffsetX()][y + o.getOffsetY()],casesAdjacentes);
+            }
+        }
     }
 }
